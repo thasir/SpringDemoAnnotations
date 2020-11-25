7327c345047446e236da939d39a23cb75e836e55
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-11-25 13:55:18 +0530

    Spring config with java annotations-dependency injection- Constructor Injection

--- SpringDemoAnnotations/src/springdemo/TennisCoach.java
+++ SpringDemoAnnotations/src/springdemo/TennisCoach.java
@@ -1,14 +1,28 @@
 package springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
+
+	private FortuneService fortuneService;
+
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		
+
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+
+		return fortuneService.getFortune();
 	}
 
 }
